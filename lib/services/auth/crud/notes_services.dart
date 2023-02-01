d6110290d682b31514893cc3071ccfdd460b89fa
eldosagai <eldarasanbakov@mail.ru>
2023-02-01 17:57:25 +0600

    made notes creating view

--- lib/services/auth/crud/notes_services.dart
+++ lib/services/auth/crud/notes_services.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/rendering.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
